c65426fb5d70ffb4f998663bfbc29fd3c4cdca42
A-Cobra <conradbravina@gmail.com>
2023-10-31 08:51:33 -0500

    Finished the backend
    I will refactor to controllers

--- fullstack-ecommerce-v1/backend/routes/product.route.js
+++ fullstack-ecommerce-v1/backend/routes/product.route.js
@@ -19,6 +19,39 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (!id) {
+    return badRequest(req, res);
+  }
+
+  const sqlQuerie = `SELECT * FROM ${config.productTableName} WHERE id=? LIMIT 1`;
+  // const sqlQuerie = `SELECT * FROM ${config.productTableName} WHERE id = ${id}`; //Unsafe
+
+  db.query(sqlQuerie, id, (err, result) => {
+    if (err) {
+      return internalServerError(req, res);
+    }
+
+    const records = result;
+    console.log(records);
+
+    if (records.length === 0) {
+      return res.status(404).json({
+        msg: "User not found",
+        success: false,
+      });
+    }
+
+    return res.status(201).json({
+      msg: "Product retrieved correctly",
+      success: true,
+      data: records[0],
+    });
+  });
+});
+
 router.post("/", (req, res) => {
   const { name, description, price } = req.body;
   if (!(name && description && price)) {
@@ -35,6 +68,52 @@ router.post("/", (req, res) => {
       ? res.status(500).json({ msg: "Product creation failed", success: false })
       : res.status(201).json({
           msg: "Product added correctly",
+          success: true,
+        });
+  });
+});
+
+router.patch("/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (!id) {
+    return badRequest(req, res, "The id is required");
+  }
+
+  const updatedFields = req.body;
+  const sqlQuerie = `UPDATE ${config.productTableName} SET ? WHERE id = ?`;
+
+  const payload = [updatedFields, id];
+  db.query(sqlQuerie, payload, (err) => {
+    return err
+      ? res.status(500).json({
+          msg: "Product update failed, try again later",
+          success: false,
+        })
+      : res.status(201).json({
+          msg: "Product updated correctly",
+          success: true,
+        });
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (!id) {
+    return badRequest(req, res, "The id is required");
+  }
+
+  const sqlQuerie = `DELETE FROM ${config.productTableName} WHERE id = ?`;
+
+  db.query(sqlQuerie, id, (err) => {
+    return err
+      ? res.status(500).json({
+          msg: "Product removal failed, try again later",
+          success: false,
+        })
+      : res.status(201).json({
+          msg: "Product removed correctly",
           success: true,
         });
   });
