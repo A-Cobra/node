aec4201b0c74a866c0f9f21397f04d8d937a7cc3
A-Cobra <conradbravina@gmail.com>
2023-11-16 07:36:39 -0500

    Added a not found module and a component as well

--- fullstack-ecommerce-v1/frontend/src/app/app-routing.module.ts
+++ fullstack-ecommerce-v1/frontend/src/app/app-routing.module.ts
@@ -3,9 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'products',
+  },
+  {
     path: 'products',
     loadChildren: () =>
       import('./products/products.module').then(m => m.ProductsModule),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./not-found/not-found.module').then(m => m.NotFoundModule),
   },
 ];
 
