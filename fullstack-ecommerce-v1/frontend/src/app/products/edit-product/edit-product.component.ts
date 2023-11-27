4d9af3b8e00d1ff539455c4c53b0355349dc7aa3
A-Cobra <conradbravina@gmail.com>
2023-11-27 07:31:08 -0500

    The notifications work great

--- fullstack-ecommerce-v1/frontend/src/app/products/edit-product/edit-product.component.ts
+++ fullstack-ecommerce-v1/frontend/src/app/products/edit-product/edit-product.component.ts
@@ -4,6 +4,12 @@ import { ProductsService } from '../services/product/products.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EditCreateProductPayload } from 'src/app/models/edit-create-product-payload.interface';
 import { ProductEvent } from 'src/app/models/product-event.interface';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-edit-product',
@@ -13,13 +19,30 @@ import { ProductEvent } from 'src/app/models/product-event.interface';
 export class EditProductComponent implements OnInit {
   hasWrongPath = false;
   isFetchingProcessStarted = false;
+  wasNotFound = false;
 
   params: any;
   product!: Product;
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  message = {
+    notFound: 'Sorry, the product was not found',
+    success: (productName: string) =>
+      `Product ${productName} created successfully`,
+    routeParamsError:
+      'There was an error with the Angular Router, please try again',
+    editError:
+      'There was an error editing the note, please try again or refresh',
+    successfulEdit: (productName: string) =>
+      `Product ${productName} successfully updated`,
+  };
+
   constructor(
     private productsService: ProductsService,
     private activatedRoute: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -34,31 +57,87 @@ export class EditProductComponent implements OnInit {
         this.params = routeParams;
         this.productsService.getProductById(productId).subscribe({
           next: apiResponse => {
-            console.log(apiResponse);
             if (apiResponse?.data) {
               this.product = apiResponse.data;
             }
           },
           error: err => {
-            console.log('error');
-            console.log(err);
+            this.wasNotFound = true;
+            const snackBarRef = this.snackBar.open(
+              this.message.notFound,
+              'Go Home',
+              {
+                duration: 3500,
+              }
+            );
+            snackBarRef
+              .onAction()
+              .pipe(take(1))
+              .subscribe({
+                next: () => this.goHome(),
+              });
           },
         });
       },
-      error: err => console.log(err),
+      error: () => {
+        this.snackBar.open(this.message.routeParamsError, undefined, {
+          duration: 3500,
+        });
+      },
     });
   }
+
   onFormSubmission(editProductPayload: ProductEvent<EditCreateProductPayload>) {
-    if (window.confirm('Are you sure you want to update the product?')) {
-      try {
-        this.productsService.patchProductById(
-          editProductPayload.payload.id,
-          editProductPayload.payload.optional
-        );
-        this.router.navigate(['/products']);
-      } catch (error) {
-        console.log('ERROR editing product');
+    const snackBarRef = this.snackBar.open(
+      'Are you sure you want to update the product?',
+      'YES',
+      {
+        duration: 3500,
+        verticalPosition: this.verticalPosition,
+        horizontalPosition: this.horizontalPosition,
       }
-    }
+    );
+
+    snackBarRef
+      .onAction()
+      .pipe(take(1))
+      .subscribe({
+        next: () => {
+          try {
+            this.productsService.patchProductById(
+              editProductPayload.payload.id,
+              editProductPayload.payload.optional
+            );
+            this.snackBar.open(
+              this.message.successfulEdit(
+                editProductPayload.payload.optional.name
+              ),
+              undefined,
+              { duration: 3500 }
+            );
+            this.goHome();
+          } catch (error) {
+            const snackBarRef = this.snackBar.open(
+              this.message.editError,
+              'Go Home',
+              {
+                duration: 3500,
+                horizontalPosition: this.horizontalPosition,
+                verticalPosition: this.verticalPosition,
+              }
+            );
+            snackBarRef
+              .onAction()
+              .pipe(take(1))
+              .subscribe({
+                next: () => this.goHome(),
+              });
+          }
+        },
+      });
+  }
+
+  goHome(): void {
+    this.router.navigate(['/products']);
   }
 }
--- fullstack-ecommerce-v1/frontend/src/app/products/visualize-products/visualize-products.component.ts
+++ fullstack-ecommerce-v1/frontend/src/app/products/visualize-products/visualize-products.component.ts
@@ -5,6 +5,11 @@ import { ApiResponse } from 'src/app/models/api-response.interface';
 import { Product } from 'src/app/models/product.interface';
 import { ProductEvent } from 'src/app/models/product-event.interface';
 import { Router } from '@angular/router';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-visualize-products',
@@ -12,6 +17,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./visualize-products.component.scss'],
 })
 export class VisualizeProductsComponent implements OnInit {
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+
+  message = {
+    success: (productName: number) =>
+      `Product with id ${productName} deleted successfully`,
+    error: 'There was a problem deleting the product, please try again later',
+  };
+
   productsData$!: Observable<ApiResponse<Product[]>>;
   prods: Product[] = [
     { id: 1, name: 'myName', description: 'anyDesc', price: 14 },
@@ -19,7 +33,8 @@ export class VisualizeProductsComponent implements OnInit {
 
   constructor(
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -37,15 +52,33 @@ export class VisualizeProductsComponent implements OnInit {
   redirectToEditProductPage(productId: number): void {
     this.router.navigate(['/products', 'edit', productId]);
   }
+
   deleteProduct(productId: number): void {
-    if (window.confirm('DO you really want to delete the product?')) {
-      try {
-        this.productsService.deleteProductById(productId);
-        this.updateProducts();
-      } catch (error) {
-        console.log('Error deleting product');
+    const snackBarRef = this.snackBar.open(
+      'Do you really want to delete the product?',
+      'YES',
+      {
+        duration: 4000,
+        verticalPosition: this.verticalPosition,
+        horizontalPosition: this.horizontalPosition,
       }
-    }
+    );
+
+    snackBarRef.onAction().subscribe({
+      next: () => {
+        try {
+          this.productsService.deleteProductById(productId);
+          this.updateProducts();
+          this.snackBar.open(this.message.success(productId), undefined, {
+            duration: 3500,
+          });
+        } catch (error) {
+          this.snackBar.open(this.message.error, undefined, {
+            duration: 3500,
+          });
+        }
+      },
+    });
   }
 
   updateProducts(): void {
