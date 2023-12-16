f26ca039f83bf525aa54bfd6557aa02fefeb9bfe
A-Cobra <conradbravina@gmail.com>
2023-12-16 18:18:05 -0500

    Tried to add a guard bu it is not working

--- fullstack-ecommerce-v1/frontend/src/app/app-routing.module.ts
+++ fullstack-ecommerce-v1/frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IsLoggedInGuard } from './guards/is-logged-in.guard';
 
 const routes: Routes = [
   {
@@ -11,6 +12,11 @@ const routes: Routes = [
     path: 'products',
     loadChildren: () =>
       import('./products/products.module').then(m => m.ProductsModule),
+  },
+  {
+    path: 'auth',
+    canLoad: [IsLoggedInGuard],
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
   },
   {
     path: '**',
--- fullstack-ecommerce-v1/frontend/src/app/auth/services/auth.service.ts
+++ fullstack-ecommerce-v1/frontend/src/app/auth/services/auth.service.ts
@@ -7,15 +7,24 @@ import { BehaviorSubject, Observable, of } from 'rxjs';
 export class AuthService {
   private _isLoggedIn = false;
   private isLoggedInSubject = new BehaviorSubject<boolean>(this._isLoggedIn);
+  private isLoggedOutSubject = new BehaviorSubject<boolean>(!this._isLoggedIn);
   isLoggedIn$ = this.isLoggedInSubject.asObservable();
+  isLoggedOut$ = this.isLoggedOutSubject.asObservable();
 
   login() {
     // TODO: Write logic
-    this.isLoggedInSubject.next(true);
+    this._isLoggedIn = true;
+    this.emitNewLoggedInOutValues();
   }
 
   logout() {
-    this.isLoggedInSubject.next(false);
+    this._isLoggedIn = false;
+    this.emitNewLoggedInOutValues();
+  }
+
+  private emitNewLoggedInOutValues() {
+    this.isLoggedInSubject.next(this._isLoggedIn);
+    this.isLoggedOutSubject.next(!this._isLoggedIn);
   }
 
   constructor() {}
